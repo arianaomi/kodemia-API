e614484f2fbf719b60aaecc95087474502e8a137
Arianaomi <arianaomi.lp@gmail.com>
2020-07-16 17:16:42 -0500

    :zap: ADD routes of mentors
    ADD routes of mentors

--- src/server.js
+++ src/server.js
@@ -5,15 +5,16 @@ const express = require('express')
 const app = express()
 
 const kodersRouter = require('./routes/koders')
+const mentorsRouter = require('./routes/mentors')
 
 app.use(express.json())
 //montando el router de koders con use
 //segundo use de use 1.middleware 2.montar
 app.use('/koders', kodersRouter)
+app.use('/mentors', mentorsRouter)
 
 app.get('/', (requeste, response) => {
   response.json({ success: true, message: 'kodemia APIv8' })
 })
-
 
 module.exports = app
