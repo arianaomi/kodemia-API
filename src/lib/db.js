571bd51670d793afb02f43b8a785f0b0e13de71c
Arianaomi <arianaomi.lp@gmail.com>
2020-07-16 17:31:48 -0500

    :lock: Add environment variables
    Add environment variables

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,9 +2,11 @@
 
 const mongoose = require('mongoose')
 
+
 const DB_USER = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_NAME = process.env.DB_NAME
+const CONN_STRING = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@kodemia-c.7peye.mongodb.net/${DB_NAME}`
 
 module.exports = () =>
   mongoose.connect(
